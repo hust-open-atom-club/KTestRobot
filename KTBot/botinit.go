309dbf1b727e735f1e1d9b68f818c62175a2c5fa
王相智 <2059970466@qq.com>
2023-11-01 15:49:25 +0800

    Commented out a portion of the code for future functionality testing.

--- KTBot/botinit.go
+++ KTBot/botinit.go
@@ -30,26 +30,26 @@ func BotInit() bool {
 
 	KTBot_DIR = dir
 	PATCH_DIR = KTBot_DIR + "/patch/"
-	SMATCH_DIR = KTBot_DIR + "/smatch/"
+	//SMATCH_DIR = KTBot_DIR + "/smatch/"
 	MAINLINE_DIR = KTBot_DIR + "/linux-master/"
-	LINUX_NEXT_DIR = KTBot_DIR + "/linux-next-master/"
+	//LINUX_NEXT_DIR = KTBot_DIR + "/linux-next-master/"
 
 	os.MkdirAll("./patch", 0777)
 	os.MkdirAll("./log", 0777)
 
 	//git clone smatch and make smatch
-	err = RunCommand(KTBot_DIR, "ls", "-l", "smatch")
-	if err != nil {
-		err = RunCommand("", "git", "clone", "git://repo.or.cz/smatch.git")
-		if err != nil {
-			log.Fatalf("smatch clone failed: %v", err)
-		}
+	// err = RunCommand(KTBot_DIR, "ls", "-l", "smatch")
+	// if err != nil {
+	// 	err = RunCommand("", "git", "clone", "git://repo.or.cz/smatch.git")
+	// 	if err != nil {
+	// 		log.Fatalf("smatch clone failed: %v", err)
+	// 	}
 
-		err = RunCommand(SMATCH_DIR, "make")
-		if err != nil {
-			log.Fatalf("smatch make failed: %v", err)
-		}
-	}
+	// 	err = RunCommand(SMATCH_DIR, "make")
+	// 	if err != nil {
+	// 		log.Fatalf("smatch make failed: %v", err)
+	// 	}
+	// }
 
 	//download mainline
 	err = RunCommand(KTBot_DIR, "ls", "-l", "linux-master")
@@ -71,59 +71,59 @@ func BotInit() bool {
 		}
 
 		//set config
-		err = RunCommand(MAINLINE_DIR, "make", "defconfig")
-		if err != nil {
-			log.Fatalf("mainline make defconfig failed: %v", err)
-		}
+		// err = RunCommand(MAINLINE_DIR, "make", "defconfig")
+		// if err != nil {
+		// 	log.Fatalf("mainline make defconfig failed: %v", err)
+		// }
 
-		//smatch build
-		err = RunCommand(MAINLINE_DIR, SMATCH_DIR + "smatch_scripts/build_kernel_data.sh")
-		if err != nil {
-			log.Fatalf("smatch build mainline kernel failed: %v", err)
-		}
+		// //smatch build
+		// err = RunCommand(MAINLINE_DIR, SMATCH_DIR + "smatch_scripts/build_kernel_data.sh")
+		// if err != nil {
+		// 	log.Fatalf("smatch build mainline kernel failed: %v", err)
+		// }
 
-		err = RunCommand(MAINLINE_DIR, SMATCH_DIR + "smatch_scripts/test_kernel.sh")
-		if err != nil {
-			log.Fatalf("smatch test mainline kernel failed: %v", err)
-		}
+		// err = RunCommand(MAINLINE_DIR, SMATCH_DIR + "smatch_scripts/test_kernel.sh")
+		// if err != nil {
+		// 	log.Fatalf("smatch test mainline kernel failed: %v", err)
+		// }
 	}
 
 	//download linux-next
-	err = RunCommand(KTBot_DIR, "ls", "-l", "linux-next-master")
-	if err != nil {
-		linux_next_url := "https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/snapshot/linux-next-master.tar.gz"
-		err = RunCommand(KTBot_DIR, "wget", linux_next_url)
-		if err != nil {
-			log.Fatalf("Download linux_next failed: %v", err)
-		}
+	// err = RunCommand(KTBot_DIR, "ls", "-l", "linux-next-master")
+	// if err != nil {
+	// 	linux_next_url := "https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/snapshot/linux-next-master.tar.gz"
+	// 	err = RunCommand(KTBot_DIR, "wget", linux_next_url)
+	// 	if err != nil {
+	// 		log.Fatalf("Download linux_next failed: %v", err)
+	// 	}
 
-		err = RunCommand("", "tar", "zxvf", "linux-next-master.tar.gz")
-		if err != nil {
-			log.Fatalf("decompress linux_next failed: %v", err)
-		}
+	// 	err = RunCommand("", "tar", "zxvf", "linux-next-master.tar.gz")
+	// 	if err != nil {
+	// 		log.Fatalf("decompress linux_next failed: %v", err)
+	// 	}
 
-		err = RunCommand("", "rm", "-rf", "linux-next-master.tar.gz")
-		if err != nil {
-			log.Fatalf("delete linux_next.tar.gz failed: %v", err)
-		}
+	// 	err = RunCommand("", "rm", "-rf", "linux-next-master.tar.gz")
+	// 	if err != nil {
+	// 		log.Fatalf("delete linux_next.tar.gz failed: %v", err)
+	// 	}
 
-		//set config
-		err = RunCommand(LINUX_NEXT_DIR, "make", "defconfig")
-		if err != nil {
-			log.Fatalf("linux-next make defconfig failed: %v", err)
-		}
+	// 	//set config
+	// 	err = RunCommand(LINUX_NEXT_DIR, "make", "defconfig")
+	// 	if err != nil {
+	// 		log.Fatalf("linux-next make defconfig failed: %v", err)
+	// 	}
 
-		//smatch build
-		err = RunCommand(LINUX_NEXT_DIR, SMATCH_DIR + "smatch_scripts/build_kernel_data.sh")
-		if err != nil {
-			log.Fatalf("smatch build linux-next kernel failed: %v", err)
-		}
+	// 	//smatch build
+	// 	err = RunCommand(LINUX_NEXT_DIR, SMATCH_DIR + "smatch_scripts/build_kernel_data.sh")
+	// 	if err != nil {
+	// 		log.Fatalf("smatch build linux-next kernel failed: %v", err)
+	// 	}
 
-		err = RunCommand(LINUX_NEXT_DIR, SMATCH_DIR + "smatch_scripts/test_kernel.sh")
-		if err != nil {
-			log.Fatalf("smatch test linux-next kernel failed: %v", err)
-		}
-	}
+	// 	err = RunCommand(LINUX_NEXT_DIR, SMATCH_DIR + "smatch_scripts/test_kernel.sh")
+	// 	if err != nil {
+	// 		log.Fatalf("smatch test linux-next kernel failed: %v", err)
+	// 	}
+	//}
 
 	log.Println("Bot init success!")
  	return true
