c927c7c55ad49cd7eaa430cd2a98611eaf3ccf6d
Dongliang Mu <dzm91@hust.edu.cn>
2023-11-05 14:03:16 +0800

    renmae InputConfig to Config

--- KTBot/main.go
+++ KTBot/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"time"
-	"os"
-	"log"
-	"flag"
-	"strings"
 	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"strings"
+	"time"
 )
 
 type EmailHeader struct {
@@ -17,32 +17,32 @@ type EmailHeader struct {
 	Subject   string
 }
 
-type InputConfig struct {
+type Config struct {
 	// Email account used to receive kernel patches, e.g., "ktestrobot@126.com"
-	Username     string `json:"username"`
+	Username string `json:"username"`
 	// Password used to log in
-	Password     string `json:"password"`
+	Password string `json:"password"`
 	// whitelist only to be processed
 	WhiteLists []string `json:"whiteLists"`
 }
 
 type MailInfo struct {
-	SMTPServer   string `json:"smtpServer"`
-	SMTPPort     int    `json:"smtpPort"`
-	SMTPUsername string `json:"smtpUsername"`
-	SMTPPassword string `json:"smtpPassword"`
-	IMAPServer   string `json:"imapServer"`
-	IMAPPort     int    `json:"imapPort"`
-	IMAPUsername string `json:"imapUsername"`
-	IMAPPassword string `json:"imapPassword"`
-	WhiteLists []string `json:"whiteLists"`
+	SMTPServer   string   `json:"smtpServer"`
+	SMTPPort     int      `json:"smtpPort"`
+	SMTPUsername string   `json:"smtpUsername"`
+	SMTPPassword string   `json:"smtpPassword"`
+	IMAPServer   string   `json:"imapServer"`
+	IMAPPort     int      `json:"imapPort"`
+	IMAPUsername string   `json:"imapUsername"`
+	IMAPPassword string   `json:"imapPassword"`
+	WhiteLists   []string `json:"whiteLists"`
 }
 
 type EmailConfig struct {
-	SMTPServer   string `json:"smtpServer"`
-    SMTPPort     int    `json:"smtpPort"`
-    IMAPServer   string `json:"imapServer"`
-    IMAPPort     int    `json:"imapPort"`
+	SMTPServer string `json:"smtpServer"`
+	SMTPPort   int    `json:"smtpPort"`
+	IMAPServer string `json:"imapServer"`
+	IMAPPort   int    `json:"imapPort"`
 }
 
 var (
@@ -61,9 +61,8 @@ var emailheader EmailHeader
 var patchlist []string
 var LogMessage string
 
-
-func parseInputConfig(configFile string) MailInfo {
-	var inputConfig InputConfig
+func parseConfig(configFile string) MailInfo {
+	var config Config
 	// open config file
 	configFd, err := os.Open(configFile)
 	if err != nil {
@@ -74,15 +73,16 @@ func parseInputConfig(configFile string) MailInfo {
 	dec := json.NewDecoder(configFd)
 	// disallow any unknown fields
 	dec.DisallowUnknownFields()
-	if err = dec.Decode(&inputConfig); err != nil {
+	if err = dec.Decode(&config); err != nil {
 		log.Fatal(err)
 	}
 	// retrieve email account configuration based on domain
+	// TODO: add more configuration of email server
 	mapDomaintoEmailAccount := map[string]EmailConfig{
-		"126.com": {"smtp.126.com", 25, "imap.126.com", 993},
+		"126.com":     {"smtp.126.com", 25, "imap.126.com", 993},
 		"hust.edu.cn": {"mail.hust.edu.cn", 465, "mail.hust.edu.cn", 993},
 	}
-	data := strings.Split(inputConfig.Username, "@")
+	data := strings.Split(config.Username, "@")
 	if len(data) != 2 {
 		log.Fatalf("Please provide the correct email account, e.g., ktestrobot@126.com")
 	}
@@ -93,13 +93,13 @@ func parseInputConfig(configFile string) MailInfo {
 	mailinfo := MailInfo{
 		emailAccount.SMTPServer,
 		emailAccount.SMTPPort,
-		inputConfig.Username,
-		inputConfig.Password,
+		config.Username,
+		config.Password,
 		emailAccount.IMAPServer,
 		emailAccount.IMAPPort,
-		inputConfig.Username,
-		inputConfig.Password,
-		inputConfig.WhiteLists,
+		config.Username,
+		config.Password,
+		config.WhiteLists,
 	}
 	return mailinfo
 }
@@ -109,15 +109,15 @@ func main() {
 	if *flagConfig == "" {
 		log.Fatalf("No config file specified")
 	}
-	mailinfo := parseInputConfig(*flagConfig)
+	mailinfo := parseConfig(*flagConfig)
 	// init the environment of KTestRobot
 	BotInit()
 	for {
-		mailinfo.ReceiveEmail();
-		for _, patchname := range patchlist{
+		mailinfo.ReceiveEmail()
+		for _, patchname := range patchlist {
 			checkresult := "--- Test Result ---\n"
-			checkres:= CheckPatchAll(patchname, ChangedPath)
-			logname := patchname[:len(patchname) - 6]
+			checkres := CheckPatchAll(patchname, ChangedPath)
+			logname := patchname[:len(patchname)-6]
 			log_file, err2 := os.Create("log/" + logname)
 			if err2 != nil {
 				log.Println("open log_file: ", err2)
@@ -132,7 +132,7 @@ func main() {
 
 			checkresult += checkres
 			toSend := ChangedPath + LogMessage + checkresult
-			mailinfo.SendEmail(toSend, emailheader);
+			mailinfo.SendEmail(toSend, emailheader)
 		}
 		patchlist = patchlist[:0]
 		time.Sleep(time.Minute * 20)
